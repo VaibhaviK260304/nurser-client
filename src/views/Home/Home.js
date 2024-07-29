fcd4ef2e36ed508205c340214f5084ada5b30672
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-07-30 01:26:16 +0530

    onrender API URL added

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
 
     const loadPlants = async () =>{
         toast.loading("Loading Plants...")
-        const response = await axios.get('http://localhost:4000/plants')
+        const response = await axios.get('https://final-nursery-server.onrender.com')
 
         toast.dismiss()
         toast.success("Plants Loaded Successfully!!")
